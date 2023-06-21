1c0e5c149a70fee33cc54a61df746ed54defeb83
IlyuAfanasev <gecktor.86@mail.ru>
2023-06-21 21:05:35 +0400

    Initial commit

---
+++ src/test/java/pro/sky/java/course2/hm_2_9_spring_store/ApplicationTests.java
@@ -0,0 +1,13 @@
+package pro.sky.java.course2.hm_2_9_spring_store;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
